c2ae6d56b5ec0901e4aa28954bcd683ff310b240
hunterkepley <hkepley@redhat.com>
2025-11-09 23:09:40 -0500

    improve function values

--- bin/main.ml
+++ bin/main.ml
@@ -16,9 +16,12 @@ let () =
   let final_args = List.rev !args in
   let head_arg = Helpers.hd_safe final_args in
   (
+    (* Handle empty args *)
+    if List.is_empty !args then
+      Helpers.print_err "No arguments provided, please run './godoogle info' or './godoogle --help' for more info on usage";
     (* Info command *)
     if head_arg = Consts.info_arg then 
-      print_string (Cmd_info.Runner.run ())
+      print_string Cmd_info.Runner.run
     else ();
     
     (* Query command *)
--- lib/cmd/info/cmd_info.ml
+++ lib/cmd/info/cmd_info.ml
@@ -1,5 +1,5 @@
 module Runner = struct
-  let run () =
+  let run =
     "info        --> Displays information about Godoogle\n"^
     
     "query <URL> --> Queries a given url, in the future will be used for advanced queries, "^
